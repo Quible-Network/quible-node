291f719c4275a05534e52117040308030c0b4edb
Tenor <tenor@fmail.co.uk>
2024-08-17 20:53:52 -0400

    initial sqlite integration

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use std::sync::{Arc, Mutex};
 use std::net::SocketAddr;
+use jsonrpsee::types::error::CALL_EXECUTION_FAILED_CODE;
 use jsonrpsee::core::async_trait;
 use tokio::time::{Duration, Instant, sleep_until};
 // use jsonrpsee::core::client::ClientT;
 // use jsonrpsee::http_client::HttpClient;
 // use jsonrpsee::rpc_params;
 use jsonrpsee::{server::Server, types::ErrorObjectOwned};
+use rusqlite::{Connection, Result};
+use sha3::{Digest, Keccak256};
 
 use quible_rpc::QuibleRpcServer;
 
@@ -23,20 +27,48 @@ fn propose_block(block_number: i64) {
     // TODO(surrealdb): insert blocks into db
 }
 
-pub struct QuibleRpcServerImpl;
+pub struct QuibleRpcServerImpl {
+    db: Arc<Mutex<Connection>>
+}
 
 #[async_trait]
 impl quible_rpc::QuibleRpcServer for QuibleRpcServerImpl {
     async fn send_transaction(&self, transaction: types::Transaction) -> Result<types::Transaction, ErrorObjectOwned> {
+        let mut transaction_data_hasher = Keccak256::new();
+        for event in transaction.clone().events {
+            match event {
+                types::Event::CreateQuirkle { members, proof_ttl } => {
+                    for member in members {
+                        transaction_data_hasher.update(member);
+                    }
+                    transaction_data_hasher.update(bytemuck::cast::<u64, [u8; 8]>(proof_ttl));
+                }
+            }
+        }
+        let transaction_hash: [u8; 32]; // TODO: check that this is the right size
+        let transaction_hash_vec = transaction_data_hasher.finalize();
+        transaction_hash = transaction_hash_vec.as_slice().try_into().unwrap();
+        let transaction_json = serde_json::to_string(&transaction).unwrap();
+        let db = &self.db.lock().unwrap();
+        db.execute("
+            INSERT INTO pending_transactions (hash, data)
+            VALUES (?1, ?2)
+        ", (transaction_hash, transaction_json)).map_err(|error| {
+            ErrorObjectOwned::owned(
+                CALL_EXECUTION_FAILED_CODE,
+                "call execution failed: failed to insert",
+                Some(error.to_string())
+            )
+        })?;
         Ok(transaction)
     }
 }
 
-async fn run_derive_server() -> anyhow::Result<SocketAddr> {
+async fn run_derive_server(conn: Connection) -> anyhow::Result<SocketAddr> {
     let server = Server::builder().build("127.0.0.1:9013".parse::<SocketAddr>()?).await?;
 
     let addr = server.local_addr()?;
-    let handle = server.start(QuibleRpcServerImpl.into_rpc());
+    let handle = server.start(QuibleRpcServerImpl { db: Arc::new(Mutex::new(conn)) }.into_rpc());
 
     tokio::spawn(handle.stopped());
 
@@ -45,9 +77,18 @@ async fn run_derive_server() -> anyhow::Result<SocketAddr> {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let server_addr = run_derive_server().await?;
+    let conn = Connection::open_in_memory()?;
+    conn.execute("
+        CREATE TABLE pending_transactions (
+          hash STRING PRIMARY KEY,
+          data JSON
+        )
+    ", ())?;
+
+    let server_addr = run_derive_server(conn).await?;
     let url = format!("http://{}", server_addr);
     println!("server listening at {}", url);
+
     // TODO: move proposal loop into a thread or something async
     // TODO: handle an incoming transaction over RPC
     // TODO(surrealdb): start a transaction pool table
