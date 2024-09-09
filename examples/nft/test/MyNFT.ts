ffaa0fd71d42534d62a792758b4efa15271578d9
Tenor <tenor@fmail.co.uk>
2024-09-09 10:48:56 -0400

    test: write working unit test for MyNFT

--- examples/nft/test/MyNFT.ts
+++ examples/nft/test/MyNFT.ts
@@ -7,15 +7,17 @@ describe("MyNFT", function () {
     const [owner, otherAccount] = await hre.ethers.getSigners();
 
     const MyNFTFactory = await hre.ethers.getContractFactory("MyNFT");
-    const myNFT = await MyNFTFactory.deploy(owner);
+    const myNFT = await MyNFTFactory.deploy(owner, '0x289acd8eac89dba64b50df6342ec1d79d15994a0bd622023f5a51a1b2ab96386');
 
     return { myNFT, owner, otherAccount };
   }
 
   it("should handle safeMint", async function () {
-    const { myNFT, otherAccount } = await loadFixture(deployFixture);
+    const { myNFT } = await loadFixture(deployFixture);
 
-    await myNFT.safeMint(otherAccount);
-    expect(await myNFT.balanceOf(otherAccount)).to.equal(1);
+    const memberAddress = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+
+    await myNFT.safeMint(memberAddress, 9999999999n, "0x8b244d180756558fb47cdd3f5e17abb13badea298c17f1fa84659b6e29ef4ef822415b1288219fddd549ed8df83951f55ee9f929a136bf8f82b6f7f81b00d4b51c");
+    expect(await myNFT.balanceOf(memberAddress)).to.equal(1);
   });
 });
