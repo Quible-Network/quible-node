75b1d7404a461fdb242b4154d38b7b5ecab51ddd
Tenor <tenor@fmail.co.uk>
2024-09-13 14:10:15 -0400

    integrate js-sdk with example project

--- sdk/apps/nft-minting-example/src/components/LaunchToken.tsx
+++ sdk/apps/nft-minting-example/src/components/LaunchToken.tsx
@@ -4,47 +4,14 @@ import { useDeployContract, useConfig, useSignMessage } from "wagmi";
 import MyNFTArtifacts from "../../artifacts/contracts/MyNFT.sol/MyNFT.json";
 import Link from "next/link";
 import ReactEditList, * as REL from "react-edit-list";
-import { keccak256 } from "viem";
-import { getQuirkleRoot } from "../utils/getQuirkleRoot";
+import { QuibleProvider } from '@quible/js-sdk';
+
+const quibleProvider = new QuibleProvider('http://localhost:9013')
 
 const schema: REL.Schema = [
   { name: "id", type: "id" },
   { name: "address", type: "string" },
 ];
-
-const getTransactionHash = (members: string[], slug: string) => {
-  /*
-  const memberBytes = members.map((hex) => {
-    return hex.slice(2).match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16))
-  }).reduce((result, member) => [...result, ...member], []);
-   */
-  const encodedMembers = members.map((hex) => new TextEncoder().encode(hex))
-  const memberBytesLength = encodedMembers.reduce((acc, curr) => acc + curr.length, 0)
-  const memberBytes = new Uint8Array(memberBytesLength);
-
-  let offset = 0;
-  for (const member of encodedMembers) {
-    memberBytes.set(member, offset);
-    offset += member.length;
-  }
-
-  // const proofTTLBytes = '00000000000150A0'.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16))
-  const proofTTLBytes: number[] = []
-
-  const slugBytes = new TextEncoder().encode(slug);
-
-  const leftBytes = new Uint8Array([...memberBytes, ...proofTTLBytes]);
-  const bytes = new Uint8Array(leftBytes.length + slugBytes.length)
-  bytes.set(leftBytes, 0)
-  bytes.set(slugBytes, leftBytes.length)
-  return {
-    hash: keccak256(bytes),
-    contentBytes: bytes,
-    content: Array.from(bytes)
-        .map(byte => byte.toString(16).padStart(2, '0'))
-        .join('')
-  };
-}
 
 const LaunchToken = (props: {
   accountAddress: string;
@@ -58,58 +25,19 @@ const LaunchToken = (props: {
   const { deployContractAsync } = useDeployContract();
 
   const handleDeployContract = useCallback(async () => {
-    const slug = `${Math.random()}`
     setIsPending(true);
+    const wallet = quibleProvider.getWallet(props.accountAddress);
 
-    const { content, contentBytes, hash: quibleTransactionHash } = getTransactionHash(accessList, '');
-    console.log(
-      'got hash content',
-      content
-    );
-
-    console.log(
-      'got hash',
-      quibleTransactionHash
-    );
-
-    console.log('requesting signature', quibleTransactionHash);
-    const signature = await signMessageAsync({
-      message: { raw: contentBytes }
+    const { raw: quirkleRoot } = await wallet.createQuirkle({
+      members: accessList,
+      proofTtl: 86400,
+      signMessage: (message) => signMessageAsync({message: { raw: message }})
     })
-
-    console.log('using signature', signature);
-
-    const response = await fetch(
-      'http://localhost:9013',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          jsonrpc: '2.0',
-          method: 'quible_sendTransaction',
-          id: 67,
-          params: [
-            {
-              signature,
-              events: [
-                {name: 'CreateQuirkle', members: accessList, proof_ttl: 86400, slug}
-              ]
-            }
-          ]
-        })
-      }
-    )
-
-    const body = await response.json();
-
-    console.log('got body', body);
 
     const hash = await deployContractAsync({
       abi: MyNFTArtifacts.abi,
       bytecode: MyNFTArtifacts.bytecode as unknown as `0x${string}`,
-      args: [props.accountAddress, getQuirkleRoot(props.accountAddress, slug)],
+      args: [props.accountAddress, quirkleRoot],
     });
 
     const { contractAddress: newContractAddress } =
