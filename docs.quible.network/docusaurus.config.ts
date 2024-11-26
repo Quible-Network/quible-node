67ea6b76a4c3749f1dab1bc1d0f044ecda2ef55c
Tenor <tenor@fmail.co.uk>
2024-11-25 22:33:59 -0500

    build(docs.quible.network): integrate tailwindcss with docusaurus

--- docs.quible.network/docusaurus.config.ts
+++ docs.quible.network/docusaurus.config.ts
@@ -1,6 +1,7 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import tailwindPlugin from './plugins/tailwind-config.cjs';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -30,6 +31,10 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+
+  plugins: [
+    tailwindPlugin
+  ],
 
   presets: [
     [
